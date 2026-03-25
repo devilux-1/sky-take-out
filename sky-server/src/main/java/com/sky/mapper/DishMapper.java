4312c474c8950d0faddcea3fe2caf6aa839b8b6d
devill <3259300377@qq.com>
2026-03-25 19:31:03 +0800

    菜品分类模块功能开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,6 +1,9 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -19,4 +22,6 @@ public interface DishMapper {
     @Insert("insert into dish (name, category_id, price, image, description, create_time, update_time, create_user,update_user, status)\n" +
             "        values (#{name}, #{categoryId}, #{price}, #{image}, #{description}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser}, #{status}) ")
     void saveWithDish(Dish dish);
+
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
