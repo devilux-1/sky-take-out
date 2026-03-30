0ad040739f3acd0b79292ca9a0368f25974efa57
devill <3259300377@qq.com>
2026-03-30 16:03:49 +0800

    套餐分页查询模块功能开发

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -2,14 +2,13 @@ package com.sky.controller.admin;
 
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -23,5 +22,12 @@ public class SetmealController {
         log.info("新增套餐：{}",setmealDTO);
         setmealService.save(setmealDTO);
         return Result.success();
+    }
+
+    @GetMapping("/page")
+    public Result<PageResult> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO){
+        log.info("套餐分页查询，查询条件为：{}",setmealPageQueryDTO);
+        PageResult pageResult = setmealService.page(setmealPageQueryDTO);
+        return Result.success(pageResult);
     }
 }
--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,8 +1,11 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +23,6 @@ public interface SetmealMapper {
 
     @AutoFill(value = OperationType.INSERT)
     void save(Setmeal setmeal);
+
+    Page<SetmealVO> page(SetmealPageQueryDTO setmealPageQueryDTO);
 }
